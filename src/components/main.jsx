c8b335d669983803ef0bf872a37d069ac89e9740
Guy <ezraguy1@gmail.com>
2022-03-16 15:15:35 +0200

    generating array of drops

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -5,35 +5,50 @@ import pizza from '../images/pizza.png';
 import axios from 'axios';
 const Main = () => {
   const [bugs, setBugs] = useState(0);
+  const [percentage, setPercentage] = useState(0);
   const [loading, setLoading] = useState(true);
   const [timeLeft, setTimeLeft] = useState('');
+  const [dropArr, setDropArr] = useState([]);
+
   // Getting the amount of bugs from the API
   const getBugsAmount = () => {
     const url = 'http://www.randomnumberapi.com/api/v1.0/random?min=1&max=99&count=1';
     axios
       .get(url)
       .then((response) => {
-        // handle success
         setBugs(response?.data[0]);
+        generateDrops(response?.data[0]);
         setLoading(false);
       })
       .catch((error) => {
-        // handle error
         console.log(error);
       });
   };
-
+  const generateDrops = (bugs) => {
+    let temp = [...dropArr];
+    for (let i = 0; i < bugs; i++) {
+      temp.push({
+        index: i,
+        opacity: 1,
+        topPos: Math.floor(Math.random() * 10 + 1),
+        left: Math.floor(Math.random() * window.innerWidth + 1),
+      });
+    }
+    setDropArr(temp);
+    console.log(temp);
+  };
   // Calculating the number of minutes until 17:00
   const calcMinutes = () => {
     const now = new Date();
     const deadline = new Date();
     deadline.setHours(17, 0, 0);
-    console.log(now);
     const total = Date.parse(deadline) - now;
-    const minutesLeft = Math.floor((total / 1000 / 60) % 60);
-    const hoursLeft = Math.floor((total / (1000 * 60 * 60)) % 24);
-    setTimeLeft(`${hoursLeft} hours and ${minutesLeft} miutes`);
-    // const diff = Math.abs(new Date(today.now()) - new Date('2011/10/09 00:00'));
+    let minutesLeft = Math.floor((total / 1000 / 60) % 60);
+    let hoursLeft = Math.floor((total / (1000 * 60 * 60)) % 24);
+
+    setTimeLeft(
+      `${hoursLeft > 0 ? hoursLeft : 0} hours and ${minutesLeft > 0 ? minutesLeft : 0} minutes`
+    );
   };
   useEffect(() => {
     getBugsAmount();
@@ -41,8 +56,12 @@ const Main = () => {
   }, []);
   return (
     <div className="main">
-      <div className="main-title">{`Rainy with 23% for Pizza`}</div>
-
+      <div className="drops">
+        {dropArr.map(({ index, topPos, left, opacity }) => (
+          <div key={index} className="drop" style={{ top: topPos }}></div>
+        ))}
+      </div>
+      <div className="main-title">{`Rainy with ${percentage}% for Pizza`}</div>
       <div className="sub-title">
         <span>{loading ? <Loader /> : bugs}</span>
         {bugs > 1
